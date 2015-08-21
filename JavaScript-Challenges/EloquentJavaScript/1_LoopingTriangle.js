665d6a02179fbfcff43c0368daa53c4125ff3934
Aaron Covington <TheAaronCovington@gmail.com>
2015-08-20 20:35:01 -0500

    Update 1_LoopingTriangle.js

--- JavaScript-Challenges/EloquentJavaScript/1_LoopingTriangle.js
+++ JavaScript-Challenges/EloquentJavaScript/1_LoopingTriangle.js
@@ -1,10 +1,18 @@
 /*
+DESCRIPTION: Write a loop that makes seven calls to console.log to output the following triangle:
 
+#
+##
+###
+####
+#####
+######
+#######
 
 */
 
 var temp = "";
-for(var i = 0; i < 7; i++){
-  	var temp = temp + "#";
+for (var i = 0; i < 7; i++) {
+	var temp = temp + "#";
 	console.log(temp);
 }
